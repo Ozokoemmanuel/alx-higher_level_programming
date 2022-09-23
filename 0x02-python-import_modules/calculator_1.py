2113fc71ec2f001cdd49603bf461eec7f175dac8
Ozokoemmanuel <pcozoko@gmail.com>
2022-09-23 13:38:45 +0100

    update

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ My addition function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value is a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function
+    Args:
+    a: first integer
+    b: second
+    Return:
+    the return value is a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function
+    Args:
+    a: first integer
+    b: second
+    Return:
+    The return value is a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+    Args:
+    a: first integer
+    b: second
+    Return:
+    The return value is a / b
+    """
+    return int(a / b)
