4da55f160f59fa8286280886f08061d35fca708a
root <root@PraiseL.localdomain>
2022-10-11 05:27:21 +0100

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     start = 0
     for i in range(x):
-        try: 
+        try:
             print("{}".format(my_list[i]), end="")
         except (IndexError):
             break
