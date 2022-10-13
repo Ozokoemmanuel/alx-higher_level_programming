519f77b24cddb0a94dd163a9ef7b0eac5565963c
root <root@PraiseL.localdomain>
2022-10-13 03:37:47 +0100

    1-square.py

---
+++ 0x06-python_classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class to define a square """
+class Square:
+    """
+    Define the properties of a square
+
+    """
+    def __init__(self, size):
+        """
+        Instantate the size
+
+        """
+        self.__size = size
